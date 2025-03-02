ff3f4e0382407304eefa8eda96f11f9dd71672d1
OgradL <dragoleonardo1@gmail.com>
2025-03-02 23:40:53 +0100

    add foreign keys

--- db.py
+++ db.py
@@ -8,11 +8,11 @@ db = SQLAlchemy(app.app)
 
 class user(db.Model):
     id = Column(Integer, primary_key=True)
-    fascia1 = Column(Integer)
-    fascia2 = Column(Integer)
-    fascia3 = Column(Integer)
-    fascia4 = Column(Integer)
-    fascia5 = Column(Integer)
+    fascia1 = Column(Integer, ForeignKey("corso.id"))
+    fascia2 = Column(Integer, ForeignKey("corso.id"))
+    fascia3 = Column(Integer, ForeignKey("corso.id"))
+    fascia4 = Column(Integer, ForeignKey("corso.id"))
+    fascia5 = Column(Integer, ForeignKey("corso.id"))
 
 
 
