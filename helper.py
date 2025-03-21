c397e086d52d209f067bfdb48c937cc4d8131159
OgradL <dragoleonardo1@gmail.com>
2025-03-21 19:42:03 +0100

    fix orari_fasce format

--- helper.py
+++ helper.py
@@ -8,12 +8,22 @@ from datetime import timedelta, datetime
 EMAIL_REGEX = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
 
 orari_fasce = [
-    datetime.fromisoformat("2025-04-14T09:00"),
-    datetime.fromisoformat("2025-04-14T11:00"),
-    datetime.fromisoformat("2025-04-15T09:00"),
-    datetime.fromisoformat("2025-04-15T11:00"),
-    datetime.fromisoformat("2025-04-16T09:00")
+    "",
+    "14/04 - 09:00",
+    "14/04 - 11:00",
+    "15/04 - 09:00",
+    "15/04 - 11:00",
+    "16/04 - 09:00",
 ]
+
+# orari_fasce = [
+#     datetime.fromisoformat("2025-04-14T09:00"),
+#     datetime.fromisoformat("2025-04-14T09:00"),
+#     datetime.fromisoformat("2025-04-14T11:00"),
+#     datetime.fromisoformat("2025-04-15T09:00"),
+#     datetime.fromisoformat("2025-04-15T11:00"),
+#     datetime.fromisoformat("2025-04-16T09:00")
+# ]
 
 def login_required(f):
     def wrapped(*args, **kwargs):
