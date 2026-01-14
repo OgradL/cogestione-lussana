4c9e4dabdd6f04f7861c902874b1e0c1e9feee0d
OgradL <dragoleonardo1@gmail.com>
2026-01-14 20:32:54 +0100

    reworked login (merged login and register)

--- cogestione/auth.py
+++ cogestione/auth.py
@@ -2,7 +2,6 @@
 from flask import Blueprint, Response, render_template, flash, redirect, url_for, request, session
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
-import re
 
 
 from cogestione import utils
@@ -11,14 +10,46 @@ from cogestione import db as database
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+@bp.route("/prelogin/", methods=["GET", "POST"])
+def prelogin():
+    session.clear()
+
+    if request.method == "GET":
+        return render_template("prelogin.html")
+
+    db = database.get_db()
+    email = request.form.get("email")
+
+    if email is None:
+        return Response("bad request", 400)
+
+    email = email.strip()
+
+    user = db.session.scalar(db.select(database.user).filter_by(email=email))
+
+    if user is None:
+        flash("Email non esistente. Se si tratta di un errore, contatta i rappresentanti", 'error')
+        return redirect(url_for("auth.prelogin"))
+
+    session.permanent = True
+    session["loggin_in"] = True
+    session["email"] = email
+
+    if user.password == "":
+        return redirect(url_for("auth.register"))
+
+    return redirect(url_for("auth.login"))
+
 @bp.route("/login/", methods=["GET", "POST"])
+@utils.is_loggin
 def login():
     if request.method == "GET":
-        return render_template("login.html")
-    
+        return render_template("login.html", user_email = session["email"])
+
     db = database.get_db()
 
-    email = request.form.get("email")
+    # email = request.form.get("email")
+    email = session["email"]
     password = request.form.get("password")
 
     if email is None or password is None:
@@ -26,22 +57,23 @@ def login():
 
     email = email.strip()
     password = password.strip()
-    
-    user = db.session.scalar(db.select(database.user).filter_by(email=email))
-    
+
+    user = db.session.scalar(db.select(database.user).where(database.user.email == email))
+
     if user is None:
         flash("Email o password errati", 'error')
         return redirect(url_for("auth.login"))
-    
+
     if not check_password_hash(user.password, password):
         flash("Email o password errati", 'error')
         return redirect(url_for("auth.login"))
 
+    session.clear()
     session.permanent = True
     session["user_id"] = user.id
     session["email"] = email
     session["logged"] = True
-    
+
     flash("Login effettuato con successo", 'success')
     return redirect(url_for("core.home"))
 
@@ -66,14 +98,14 @@ Subject: {subject}
 
 def send_auth_verification_email(email):
     testo = f"""Questo è il tuo codice di verififca:\n\t{session["auth_code"]}\n\nInseriscilo nella pagina di registrazione!"""
-    
+
     send_email(email,
                "Codice di verifica cogestione",
                testo)
 
 def send_pwd_reset_email(email):
     testo = f"""Questo è il tuo codice di verifica:\n\t{session["auth_code"]}\n\nInseriscilo nella pagina di reset della password!"""
-    
+
     send_email(email, "Codice di verifica cogestione", testo)
 
 
@@ -81,17 +113,17 @@ def send_pwd_reset_email(email):
 def verification():
     if session.get("auth_code", -1) == -1 or session.get("auth_age", -1) == -1:
         return redirect(url_for("core.home"))
-    
+
     if (datetime.now() - datetime.fromisoformat(session["auth_age"])).seconds >= 600:
         session.clear()
-    
+
     if session.get("auth_code", -1) == -1 or session.get("auth_age", -1) == -1:
         return redirect(url_for("core.home"))
-    
-    
+
+
     if request.method == "GET":
         return render_template("verification.html")
-    
+
     db = database.get_db()
 
     code = request.form.get("verification-code")
@@ -100,80 +132,78 @@ def verification():
         return Response("bad request", 400)
 
     code = code.strip()
-    
+
     if code != session["auth_code"]:
         session.clear()
         flash("Il codice di controllo è sbagliato", 'error')
         return redirect(url_for("auth.register"))
-    
+
     email = session["tmp_email"]
     password = session["tmp_password"]
     session.clear()
-    
+
     user = db.session.scalar(db.select(database.user).filter_by(email=email))
     user.password = password
-    
+
     db.session.commit()
-    
+
     flash("Registrato con successo", 'success')
     return redirect(url_for("auth.login"))
 
 @bp.route("/register/", methods=["GET", "POST"])
+@utils.is_loggin
 def register():
     if request.method == "GET":
-        return render_template("register.html")
-    
+        return render_template("register.html", user_email = session["email"])
+
     failed = False
 
     db = database.get_db()
-    
-    email = request.form.get("email")
+
+    # email = request.form.get("email")
+    email = session["email"]
     password1 = request.form.get("password1")
     password2 = request.form.get("password2")
-    
+
     if email is None or password1 is None or password2 is None:
         return Response("bad request", 400)
 
     email = email.strip()
     password1 = password1.strip()
     password2 = password2.strip()
-    
-    if re.match(utils.EMAIL_REGEX, email) is None:
-        flash("Email non valida", 'error')
-        failed = True
 
     if not email.endswith("@liceolussana.eu"):
         flash("Devi usare l'email istituzionale (...@liceolussana.eu)")
         failed = True
-    
+
 
     # more checks
-    
-    user = db.session.scalar(db.select(database.user).filter_by(email=email))
-    
+
+    user = db.session.scalar(db.select(database.user).where(database.user.email == email))
+
     if user is None:
         flash("L'email non è esistente. Se credi ci sia stato un errore, contatta i rappresentanti", "error")
         failed = True
-        return redirect(url_for("auth.register"))    
+        return redirect(url_for("auth.register"))
 
     if user.password != "":
         flash("L'email è già registrata", 'error')
-    
+
     if password1 != password2:
         flash("Le password non corrispondono", 'error')
         failed = True
 
     if failed:
         return redirect(url_for("auth.register"))
-    
+
     session.clear()
     session["auth_code"] = utils.generate_auth_code(6)
     session["auth_age"] = datetime.now().isoformat()
-    
+
     flash(f"Ti abbiamo inviato una mail di verifica su {email}")
-    
+
     send_auth_verification_email(email)
-    
+
     password = generate_password_hash(password1)
 
     session["tmp_email"] = email
@@ -186,17 +216,17 @@ def register():
 def verification_reset_pwd():
     if session.get("auth_code", -1) == -1 or session.get("auth_age", -1) == -1:
         return redirect(url_for("core.home"))
-    
+
     if (datetime.now() - datetime.fromisoformat(session["auth_age"])).seconds >= 600:
         session.clear()
-    
+
     if session.get("auth_code", -1) == -1 or session.get("auth_age", -1) == -1:
         return redirect(url_for("core.home"))
-    
-    
+
+
     if request.method == "GET":
         return render_template("verification-reset-pwd.html")
-    
+
     db = database.get_db()
 
     code = request.form.get("verification-code")
@@ -205,23 +235,23 @@ def verification_reset_pwd():
         return Response("bad request", 400)
 
     code = code.strip()
-    
+
     if code != session["auth_code"]:
         session.clear()
         flash("Il codice di controllo è sbagliato", 'error')
         return redirect(url_for("auth.reset_password"))
-    
+
     email = session["tmp_email"]
     password = session["tmp_password"]
     session.clear()
-    
+
     user = db.session.scalar(db.select(database.user).filter_by(email=email))
 
     user.password = password
     db.session.commit()
 
     session.clear()
-    
+
     flash("Password modificata con successo", 'success')
     return redirect(url_for("auth.login"))
 
@@ -229,47 +259,47 @@ def verification_reset_pwd():
 def reset_password():
     if request.method == "GET":
         return render_template("reset-pwd.html")
-    
+
     failed = False
 
     db = database.get_db()
-    
+
     email = request.form.get("email")
     password1 = request.form.get("password1")
     password2 = request.form.get("password2")
 
     if email is None or password1 is None or password2 is None:
         return Response("bad request", 400)
-    
+
     email = email.strip()
     password1 = password1.strip()
     password2 = password2.strip()
 
     # more checks
-    
+
     user = db.session.scalar(db.select(database.user).filter_by(email=email))
-    
+
     if user is None:
         flash("L'email non è valida", "error")
         failed = True
-    
+
     if password1 != password2:
         flash("Le password non corrispondono", 'error')
         failed = True
 
     if failed:
         return redirect(url_for("auth.reset-password"))
-    
+
     session.clear()
     session["auth_code"] = utils.generate_auth_code(6)
     session["auth_age"] = datetime.now().isoformat()
-    
+
     flash(f"Ti abbiamo inviato una mail di verifica su {email}")
-    
+
     send_pwd_reset_email(email)
-    
+
     password = generate_password_hash(password1)
-    
+
     session["tmp_email"] = email
     session["tmp_password"] = password
 
--- cogestione/utils.py
+++ cogestione/utils.py
@@ -6,9 +6,18 @@ import os
 
 EMAIL_REGEX = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
 
+def is_loggin(f):
+    def wrapped(*args, **kwargs):
+        if "loggin_in" in session and session["loggin_in"]:
+            return f(*args, **kwargs)
+        return redirect(url_for('auth.prelogin'))
+    wrapped.__name__ = f.__name__
+    return wrapped
+
+
 def login_required(f):
     def wrapped(*args, **kwargs):
-        if 'email' in session:
+        if "logged" in session and session["logged"]:
             return f(*args, **kwargs)
         flash("Devi aver fatto il login!", 'error')
         return redirect(url_for('auth.login'))
